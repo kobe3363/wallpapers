559b8e8e533c008001e787c91cc7c85dd0b03e27
kobe3363 <kobe3363@gmail.com>
2026-01-13 00:00:06 +0200

    pw codegen
    Introduces an automated test that verifies searching for wallpapers by keyword, distinguishing free vs premium items, and downloading a free wallpaper using Playwright. The test covers navigation, interaction with UI elements, and download verification.

---
+++ tests/search.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    // searching for wallpapers by keyword.
+    await page.goto(process.env.BASE_URL as string);
+    await page.getByRole('button', { name: 'Reject Optional Cookies' }).click();
+    await page.getByRole('link', { name: 'Browse Now' }).click();
+    await page.getByRole('navigation').getByRole('button', { name: 'All' }).click();
+    await page.getByLabel('All').getByText('Wallpapers', { exact: true }).click();
+    await page.getByRole('navigation').getByRole('textbox', { name: 'Search ' + process.env.COMPANY_NAME }).click();
+    await page.getByRole('navigation').getByRole('textbox', { name: 'Search ' + process.env.COMPANY_NAME }).fill('sunrise');
+    // identifying free vs premium
+    await page.getByRole('navigation').getByRole('button', { name: 'Search' }).click();
+    await page.getByRole('button', { name: 'Price' }).click();
+    // downloading free wallpaper
+    await page.locator('.Checkbox_input__ZoALC').first().click();
+    await expect(page.getByRole('button', { name: 'Free' })).toBeVisible();
+    await page.getByRole('link', { name: 'sunrise' }).first().click();
+    await expect(page.getByRole('button', { name: 'Download' })).toBeVisible();
+    await page.getByRole('button', { name: 'Download' }).click();
+    await expect(page.getByRole('button', { name: 'Close' })).toBeVisible();
+    await page.getByText('14').click();
+    // We should verify that the wallpaper item was successfully downloaded as a final step.
+    // Create test suit for automated tests
+    const downloadPromise = page.waitForEvent('download');
+    await page.goto(process.env.BASE_URL + '/wallpapers/0f377020-bbc0-47fb-ac5a-ade2429217ac');
+    const download = await downloadPromise;
+});
