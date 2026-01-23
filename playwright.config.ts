d9ea738c35c88e10b5f380af71424bf8185e6a08
kobe3363 <kobe3363@gmail.com>
2026-01-23 16:52:29 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ const COMMON_USE: PlaywrightTestConfig['use'] = {
   trace: 'on-first-retry',
   video: 'retain-on-failure',
   screenshot: 'only-on-failure',
-  headless: !!process.env.CI,
+  headless: true, //!!process.env.CI,
   extraHTTPHeaders: {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     'Accept-Language': 'en-US,en;q=0.9',
